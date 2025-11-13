0ba678a885d99ed8e86403c0cb49a4af7558d710
sakshi <sakshi.patel@shriffle.com>
2025-11-13 19:44:08 +0530

    Added route level middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,8 @@
 const authMiddleware=(req, res, next)=>{
+  const authorized = true;
+  if (!authorized) {
+    return res.status(403).json({ message: 'Forbidden' });
+  }
   next();
 }
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { getAllUser, addUser, deleteUser, updatedUser, getUserById } = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.get("/", getAllUser);
+router.get("/", authMiddleware,getAllUser);
 router.post("/add", addUser);
 router.put('/:id', updatedUser);
 router.delete('/:id', deleteUser);
